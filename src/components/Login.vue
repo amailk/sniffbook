f520c0abc27dc446372d0f65f882019f35d85333
Ama Liyanage <liyaniama@gmail.com>
2020-03-03 21:40:11 -0500

    Add Buefy and create skeleton login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,19 @@
 <template>
-    <div>
-        Login
+    <div class="container">
+         <b-field label="Username"
+            type="is-success"
+            message="This username is available">
+            <b-input value="johnsilver" maxlength="30"></b-input>
+        </b-field>
+
+        <b-field label="Password">
+            <b-input type="password"
+                value="iwantmytreasure"
+                password-reveal>
+            </b-input>
+        </b-field>  
+
+         <b-button type="is-primary">Login</b-button>
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Buefy from 'buefy'
+
 import App from './App.vue'
 
 import routes from './routes';
 
-
 Vue.config.productionTip = false;
 Vue.use(VueRouter)
+Vue.use(Buefy)
 
 const router = new VueRouter({mode: 'history', routes})
 
