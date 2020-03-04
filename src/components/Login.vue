0a1f6ec27439f510ec3820cf542036b0c0238f43
Ama Liyanage <liyaniama@gmail.com>
2020-03-03 21:56:32 -0500

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,19 +1,25 @@
 <template>
-    <div class="container">
-         <b-field label="Username"
-            type="is-success"
-            message="This username is available">
-            <b-input value="johnsilver" maxlength="30"></b-input>
-        </b-field>
+    <div class="columns is-vcentered is-centered">
+        <div class="column is-one-third">
+            <b-field label="Username"
+                type="is-success"
+                message="This username is available">
+                <b-input value="johnsilver" maxlength="30"></b-input>
+            </b-field>
 
-        <b-field label="Password">
-            <b-input type="password"
-                value="iwantmytreasure"
-                password-reveal>
-            </b-input>
-        </b-field>  
+            <b-field label="Password">
+                <b-input type="password"
+                    value="iwantmytreasure"
+                    password-reveal>
+                </b-input>
+            </b-field>
 
-         <b-button type="is-primary">Login</b-button>
+            <b-field horizontal>
+                <p class="control">
+                    <button class="button is-primary">Login</button>
+                </p>
+            </b-field>
+        </div>
     </div>
 </template>
 
